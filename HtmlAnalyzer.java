fa625abf9b188c51270efd2be3027b97cbbe84f6
Pedro Zanatta <pedrohenriquezanattadeoliveira@gmail.com>
2026-02-08 19:06:34 -0300

    feature: connecting url with http method

--- HtmlAnalyzer.java
+++ HtmlAnalyzer.java
@@ -1,14 +1,32 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class HtmlAnalyzer {
 
     public static String ERROR_MESSAGE = "URL connection error";
 
     public static void main(String[] args) {
-        if (args.length == 1){
-            System.out.println("Funcionou");
-        } else {
+        if (args.length != 1) {
             System.out.println(ERROR_MESSAGE);
-            System.exit(1);
+            return;
+        }
+
+        String urlString = args[0];
+
+        try{
+            URL url = new URL(urlString);
+            HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+            urlConn.setRequestMethod("GET");
+
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(urlConn.getInputStream(), StandardCharsets.UTF_8));
+            Scanner scanner = new Scanner(buffer);
+
+        } catch(IOException ioe){
+            System.out.println(ERROR_MESSAGE);
         }
     }
 }
