537816bfc69d91531a54bd2c678cf09e334d9d89
Pedro Zanatta <pedrohenriquezanattadeoliveira@gmail.com>
2026-02-09 17:14:33 -0300

    feature: implemented tag control with Stack and some logical bugs fixed

--- HtmlAnalyzer.java
+++ HtmlAnalyzer.java
@@ -4,11 +4,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class HtmlAnalyzer {
 
     public static String ERROR_MESSAGE = "URL connection error";
-    public static String MALFORMED_URL = "malformed url";
+    public static String MALFORMED_HTML = "malformed HTML";
 
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -24,28 +25,44 @@ public class HtmlAnalyzer {
             urlConn.setRequestMethod("GET");
             int responseCode = urlConn.getResponseCode();
 
-            if(responseCode != 200) {
+            if(responseCode == HttpURLConnection.HTTP_OK) {
                 BufferedReader buffer = new BufferedReader(new InputStreamReader(urlConn.getInputStream(), StandardCharsets.UTF_8));
-                Scanner scanner = new Scanner(buffer);
 
+                Stack<String> stack = new Stack<>();
+                String line;
                 String deeptestText = "";
                 int depth = 0;
                 int maxDepth = -1;
 
-                while (scanner.hasNextLine()) {
-
-                    String line = scanner.nextLine().trim();
+                while ((line = buffer.readLine()) != null) {
+                    line = line.trim();
 
                     if (line.isEmpty()) continue;
 
                     if (line.startsWith("</")) {
+                        String closeTag = extractTagName(line);
+
+                        if(stack.isEmpty()){
+                            System.out.println(MALFORMED_HTML);
+                            return;
+                        }
+
+                        String openTag = stack.pop();
+
+                        if(!openTag.equals(closeTag)){
+                            System.out.println(MALFORMED_HTML);
+                            return;
+                        }
+
                         depth--;
-                        if (depth < 0) {
-                            System.out.println(MALFORMED_URL);
+                        if(depth < 0){
+                            System.out.println(MALFORMED_HTML);
                             return;
                         }
 
                     } else if (line.startsWith("<")) {
+                        String tagContent = extractTagName(line);
+                        stack.push(tagContent);
                         depth++;
 
                     } else {
@@ -54,12 +71,10 @@ public class HtmlAnalyzer {
                             deeptestText = line;
                         }
                     }
-
-                    scanner.close();
                 }
 
-                if (depth != 0) {
-                    System.out.println(MALFORMED_URL);
+                if (depth != 0 || !stack.isEmpty()) {
+                    System.out.println(MALFORMED_HTML);
                     return;
                 }
 
@@ -68,12 +83,21 @@ public class HtmlAnalyzer {
             } else
                 System.out.println(ERROR_MESSAGE);
 
-        } catch (MalformedURLException err){
-            System.out.println(MALFORMED_URL);
-            System.exit(1);
+        } catch(MalformedURLException err){
+            System.out.println(ERROR_MESSAGE);
+            return;
         } catch(IOException err){
             System.out.println(ERROR_MESSAGE);
-            System.exit(1);
+            return;
         }
+    }
+
+    private static String extractTagName(String line){
+        if(line.startsWith("</"))
+            return line.substring(2, line.length() - 1);
+        else if (line.startsWith("<"))
+            return line.substring(1, line.length() - 1);
+
+        return null;
     }
 }
